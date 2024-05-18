5c583592f508bb0164eea193e2c748d8d49cd61b
Kleinccx <jarredsaludaga@gmail.com>
2024-05-19 03:09:57 +0800

    - Client and Server

---
+++ messenger/Messenger.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dotted.messenger;
+
+/**
+ *
+ * @author Klein
+ */
+public class Messenger {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
